ddb81f73736d589b35971a9d2c9b3cf3152f2921
Arash Rowshan <arash.rowshan@gmail.com>
2018-03-31 10:52:29 -0400

    interfaces

--- Arrays/src/com/arash/Main.java
+++ Arrays/src/com/arash/Main.java
@@ -1,33 +1,13 @@
 package com.arash;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.ListIterator;
-
 public class Main {
 
     public static void main(String[] args) {
-
-
-        Song song1 = new Song("desert rose", 120);
-        Song song2 = new Song( "the wall", 200);
-        ArrayList songs = new ArrayList<Song>();
-        songs.add(song1);
-        songs.add(song2);
-
-        Album album = new Album(songs, "super album");
-
-        LinkedList playlist = new LinkedList();
-
-        playlist.add(album.addSong("the wall"));
-
-        ListIterator playlistIter = playlist.listIterator();
-        playlistIter.remove();
-//        playlistIter.add();
-
-
-        System.out.println( playlistIter.next().toString());
-
+        ITelephone timsPhone;
+        timsPhone = new DeskPhone(123456);
+        timsPhone.powerOn();
+        timsPhone.callPhone(123456);
+        timsPhone.answer();
 
     }
 }
